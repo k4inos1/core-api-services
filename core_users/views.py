796ad340f8536132d661d2cd93e0cc59965d2bec
k4inos1 <ricardo.sanhueza09@inacapmail.cl>
2026-05-12 20:09:49 -0400

    chore: fix pylint/flake8 import issues

--- core_users/views.py
+++ core_users/views.py
@@ -1,5 +1,3 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
